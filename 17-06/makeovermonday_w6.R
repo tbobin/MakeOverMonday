00a6a20c610e285ae20a7dc4327b5a5f47b9f2e3
Tomasz Bobinski <tbobin@users.noreply.github.com>
2017-02-07 07:56:50 +0100

    Update makeovermonday_w6.R

--- 17-06/makeovermonday_w6.R
+++ 17-06/makeovermonday_w6.R
@@ -4,9 +4,10 @@ library(RSocrata)
 AppToken <- "JCVQmBHeUpEHN6j8CJycdJok2"
 
 
-queryurl <- "https://data.cityofchicago.org/resource/wrvz-psew.json?
-  $where=trip_start_timestamp between '2016-01-01T00:00:01' and '2016-01-01T12:00:00'&$limit=10000"
+queryurl <- "https://data.cityofchicago.org/resource/wrvz-psew.csv?$limit=10000&$where=trip_start_timestamp between '2016-01-01T12:00:01' and '2016-01-01T13:00:00'"
 
-df <- read.socrata(url = queryurl,
+validateUrl(queryurl2, AppToken)
+
+df <- read.socrata(url = queryurl2, email= EMAIL, password = PWD,
                    app_token = AppToken)
 
