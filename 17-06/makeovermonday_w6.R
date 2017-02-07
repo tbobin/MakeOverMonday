94135d59c441a7222b3fb1a05e126b02dc840a2f
Domaeg <tbobin@users.noreply.github.com>
2017-02-07 22:07:20 +0100

    connection to chicago and some json experiments

--- 17-06/makeovermonday_w6.R
+++ 17-06/makeovermonday_w6.R
@@ -1,13 +1,29 @@
 
 library(RSocrata)
+library(httr)
+library(jsonlite)
 
-AppToken <- "JCVQmBHeUpEHN6j8CJycdJok2"
 
 
-queryurl <- "https://data.cityofchicago.org/resource/wrvz-psew.csv?$limit=10000&$where=trip_start_timestamp between '2016-01-01T12:00:01' and '2016-01-01T13:00:00'"
+qurl_base <- "https://data.cityofchicago.org/resource/wrvz-psew.json?"
+qurl_limit <- "$limit=5"
+qurl_where <- "$where=trip_start_timestamp between '2016-01-01T00:00:01' and '2016-01-02T00:00:00'"
 
-validateUrl(queryurl2, AppToken)
 
-df <- read.socrata(url = queryurl2, email= EMAIL, password = PWD,
+qurl_count <- "$select=count(trip_id)"
+
+
+queryurl <- paste0(qurl_base,qurl_count,"&",qurl_where)
+queryurl <- paste0(qurl_base,qurl_limit,"&",qurl_where)
+
+valid_url <- validateUrl(queryurl, AppToken)
+
+GET(valid_url)
+
+a <- read_json(GET(valid_url))
+
+df <- read.socrata(url = queryurl, 
+                   email= EMAIL, 
+                   password = PWD,
                    app_token = AppToken)
 
