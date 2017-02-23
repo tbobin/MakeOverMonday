fced0aa9040ccab89c0e7311b9efd693ee5634e1
Domaeg <tbobin@users.noreply.github.com>
2017-02-23 21:02:43 +0100

    test animation and tweenr

--- 17-08/makeovermondayW8.R
+++ 17-08/makeovermondayW8.R
@@ -4,6 +4,8 @@ library(openxlsx)
 library(tidyr)
 library(dplyr)
 library(ggplot2)
+library(animation)
+library(tweenr)
 
 W8file <- "Potato crop yields and selling prices.xlsx"
 path <- "./17-08"
@@ -24,6 +26,40 @@ pot %>% ggplot(aes(x=Year, y=`Yield.(100.kg/ha)`, group=Country, color=Country))
   geom_line() #+ 
   # theme(legend.position = "bottom")
 
-pot %>% ggplot(aes(y=`Yield.(100.kg/ha)`, x=Area.in.1000ha, color=Country)) + geom_point() + facet_wrap(~Year)
+pot %>% ggplot(aes(y=`Yield.(100.kg/ha)`, x=Area.in.1000ha, color=Year)) + 
+  geom_point() + 
+  geom_line() +
+  facet_wrap(~Country)
 
+
+mydf <- function(yy){
+  pot.df <- pot %>% filter(Year == yy) %>% arrange(Country) %>% 
+    mutate(Land=Country, color="red") %>% select(Land,Year,`Yield.(100.kg/ha)`,Area.in.1000ha, color)
+  pot.df$`Yield.(100.kg/ha)`[is.na(pot.df$`Yield.(100.kg/ha)`)] <- 0
+  pot.df$Area.in.1000ha[is.na(pot.df$Area.in.1000ha)] <- 0
+  return(pot.df)
+}
+
+myplot <- function(i){
+  g <- tween_pot %>% filter(i == .frame) %>% 
+    ggplot(aes(y=`Yield.(100.kg/ha)`, x=Area.in.1000ha)) + 
+    geom_point() + 
+    geom_line() #+ facet_wrap(~Country)
+  return(g)
+}
+
+#pot$Country[pot$Country == "Belgium"] <- "B"
+
+plotingList <- lapply(seq(2010,2015), mydf)
+tween_pot<-tween_states(plotingList,tweenlength=2,statelength=2, ease='cubic-in-out', nframes=50)
+#tween_pot<-tween_elements(plotingList,time="Year",group="Land", ease='cubic-in-out', nframes=50)
+
+oopt = ani.options(interval = 0.1)
+saveGIF({for (i in 1:max(tween_pot$.frame)) {
+  g<-myplot(i)
+  print(g)
+  print(i)
+  ani.pause()
+}
+},movie.name="pot.gif",ani.width = 840, ani.height =450)
 
